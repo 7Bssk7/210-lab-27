f167dab25b189db98e239ff2a120d825b77f3d36
7Bssk7 <arhip.fin3@gmail.com>
2025-11-02 15:08:10 -0800

    Milestone 2

--- 210-lab-27.cpp
+++ 210-lab-27.cpp
@@ -3,6 +3,9 @@
 #include <algorithm>
 using namespace std;
 
+int mainMenu();
+void outputDetails(const map<string, tuple<int, string, string>>& );
+
 int main() {
     map<string, tuple<int, string, string>> villagerData{
         {"Drago",make_tuple(5,"Alligator", "Snap to It!")},
@@ -10,6 +13,12 @@ int main() {
         {"Raymond",make_tuple(8,"Cat", "Nice fit")}
     };
 
+    int menu = mainMenu();
+    while(menu != 4){
+        outputDetails(villagerData);
+        menu = mainMenu();
+    }
+    /*
     // Increase each villager's friendship level by 1
     for ( auto& [name,list] : villagerData){
         get<0>(list) += 1;
@@ -17,6 +26,26 @@ int main() {
     // Print the updated map
     for (const auto& [name,list] : villagerData)
         cout << name << " has a friendship level of " << get<0>(list) << endl;
-
+    */
     return 0;
+}
+
+int mainMenu(){
+    int choice;
+    cout << "1. Increase Friendship" << endl;
+    cout << "2. Decrease Friendship" << endl;
+    cout << "3. Search for Villager" << endl;
+    cout << "4. Exit" << endl;
+    cout << "Choice -> ";
+    cin >> choice;
+
+    return choice;
+
+}
+void outputDetails(const map<string, tuple<int, string, string>>& data){
+    cout << "Villager details:" << endl;
+    for (const auto& [name,list] : data){
+       cout << name << "[" << get<0>(list) << ", " << get<1>(list) << ", " << get<2>(list) << "]" << endl;
+    }
+
 }
